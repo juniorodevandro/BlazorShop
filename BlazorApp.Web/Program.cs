43b3cd7123127552532427c8271b8a427e09d229
Evandro Rode Junior <evandrorodejunior@gmail.com>
2024-06-19 21:39:08 -0300

    include MudBlazor

--- BlazorApp.Web/Program.cs
+++ BlazorApp.Web/Program.cs
@@ -2,6 +2,7 @@ using BlazorShop.Web;
 using BlazorShop.Web.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,7 +11,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 var baseUrl = "https://localhost:7237";
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl) });
 
+// MUDBLAZOR
+builder.Services.AddMudServices();
 
+// INTERFACE
 builder.Services.AddScoped<IProdutoService, ProdutoService>();
 
 await builder.Build().RunAsync();
